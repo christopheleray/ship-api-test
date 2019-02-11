5074bea18218a585f7910c49ee2a716133c6777f
christopheleray <christophe.leray1@gmail.com>
2019-02-11 18:07:10 +0100

    update routes for json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace 'api' do
-    namespace 'v1' do
+    namespace 'v1', defaults: { format: :json} do
       resources :companies, only: [:index]
       resources :products, only: [:index]
       resources :shipments, only: [:index]
